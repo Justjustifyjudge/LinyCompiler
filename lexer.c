6fef65e4390c8af1d01e1da8dbc3bb368a4a3c9e
Justjustifyjudge <1911160724@qq.com>
2024-10-21 17:23:32 +0800

    '完成简单数字词法器，待处理数字间的空格'

--- lexer.c
+++ lexer.c
@@ -1,13 +1,77 @@
 #include "compiler.h"
 #include "helpers/vector.h"
-struct lex_process* lex_process;
+#include "helpers/buffer.h"
+#include <string.h>
+
+#define LEX_GETC_IF(buffer, c, exp) \
+    for(c=peekc();exp;c=peekc()){   \
+        buffer_write(buffer, c);    \
+        nextc();                    \
+    }                               \
+
+
+static struct lex_process* lex_process;
+static struct token tmp_token;
 
 static char peekc(){
     return lex_process->functions->peek_char(lex_process);
 }
 
+static char nextc(){
+    char c=lex_process->functions->next_char(lex_process);
+    lex_process->pos.col+=1;
+    if(c=='\n'){
+        lex_process->pos.line+=1;
+        lex_process->pos.col=1;
+    }
+    return c;
+}
+
 static char pushc(char c){
     lex_process->functions->push_char(lex_process,c);
+}
+
+static struct pos lex_file_position(){
+    return lex_process->pos;
+}
+
+struct token* token_create(struct token* _token){
+    memcpy(&tmp_token,_token,sizeof(struct token));
+    tmp_token.pos=lex_file_position();
+    return &tmp_token;
+}
+
+
+
+static struct token* handle_whitespace(){
+    struct token* last_token=lexer_last_token();
+    if(last_token){
+        last_token->whitespace=true;
+    }
+    nextc();
+    return read_next_token();
+}
+
+const char* read_number_str(){
+    const char* num=NULL;
+    struct buffer* buffer=buffer_create();
+    char c=peekc();
+    LEX_GETC_IF(buffer,c,(c>='0'&&c<='9'));
+
+    buffer_write(buffer,0x00);
+    return buffer_ptr(buffer);
+}
+
+unsigned long long read_number(){
+    const char* s =read_number_str();
+    return atoll(s);
+}
+
+struct token* token_make_number_for_value(unsigned long number){
+    return token_create(&(struct token){.type=TOKEN_TYPE_NUMBER,.llnum=number});
+}
+struct token* token_make_number(){
+    return token_make_number_for_value(read_number());
 }
 
 struct token* read_next_token(){
@@ -15,13 +79,16 @@ struct token* read_next_token(){
     char c=peekc();
     switch (c)
     {
-    case EOF:
-        //结束代码文本的全部词法分析
-        break;
-    
-    default:
-        compiler_error(lex_process->compiler,"未知的字符\n");
-        break;
+        NUMERIC_CASE:
+            token=token_make_number();
+
+            break;
+        case EOF:
+            //结束代码文本的全部词法分析
+            break;
+        
+        default:
+            compiler_error(lex_process->compiler,"未知的字符\n");
     }
     return token;
 };
