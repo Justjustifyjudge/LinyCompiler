61f27c1c627ea7a54c1cc62cc70a3817c1e0c1bf
Justjustifyjudge <1911160724@qq.com>
2024-10-21 11:51:46 +0800

    '制作数字词法分析器8.551'

--- compiler.c
+++ compiler.c
@@ -1,10 +1,15 @@
 #include "compiler.h"
+#include <stdarg.h>
 
 struct lex_process_functions compiler_lex_functions={
     .next_char=compile_process_next_char,
     .peek_char=compile_process_peek_char,
     .push_char=compile_process_push_char
 };
+
+void compiler_error(struct compile_process* compiler, const char* msg, ...){
+
+}
 int compile_file(const char *filename, const char *out_filename, int flags){
     struct compile_process* process=compile_process_create(filename, out_filename, flags);
     if(!process){
--- lexer.c
+++ lexer.c
@@ -1,4 +1,42 @@
 #include "compiler.h"
+#include "helpers/vector.h"
+struct lex_process* lex_process;
+
+static char peekc(){
+    return lex_process->functions->peek_char(lex_process);
+}
+
+static char pushc(char c){
+    lex_process->functions->push_char(lex_process,c);
+}
+
+struct token* read_next_token(){
+    struct token* token=NULL;
+    char c=peekc();
+    switch (c)
+    {
+    case EOF:
+        //结束代码文本的全部词法分析
+        break;
+    
+    default:
+        compiler_error(lex_process->compiler,"未知的字符\n");
+        break;
+    }
+    return token;
+};
+
+
 int lex(struct lex_process* process){
-    return 0;
+    process->current_expression_count=0;
+    process->parentheses_buffer=NULL;
+    lex_process=process;
+    process->pos.filename=process->compiler->cfile.abs_path;
+
+    struct token* token=read_next_token();
+    while(token){
+        vector_push(process->token_vec, token);
+        token=read_next_token();
+    }
+    return LEXICAL_ANALYSIS_ALL_OK;
 }
