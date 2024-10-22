8f0cad54a37237ac555ffb3715e9bcd338ea9078
Justjustifyjudge <1911160724@qq.com>
2024-10-22 21:00:10 +0800

    '完成部分关键字识别，其他如class/struct等关键字尚未完成'

--- lexer.c
+++ lexer.c
@@ -220,12 +220,45 @@ static void lex_finish_expression(){
 bool lex_is_in_expression(){
     return lex_process->current_expression_count>0;
 }
+bool is_keyword(char* str){
+    return S_EQ(str, "unsigned")||
+           S_EQ(str, "signed")||
+           S_EQ(str, "char")||
+           S_EQ(str, "int")||
+           S_EQ(str, "short")||
+           S_EQ(str, "long")||
+           S_EQ(str, "float")||
+           S_EQ(str, "double")||
+           S_EQ(str, "void")||
+           S_EQ(str, "struct")||
+           S_EQ(str, "union")||
+           S_EQ(str, "static")||
+           S_EQ(str, "__ignore_typecheck")||
+           S_EQ(str, "return")||
+           S_EQ(str, "include")||
+           S_EQ(str, "sizeof")||
+           S_EQ(str, "if")||
+           S_EQ(str, "else")||
+           S_EQ(str, "while")||
+           S_EQ(str, "for")||
+           S_EQ(str, "do")||
+           S_EQ(str, "break")||
+           S_EQ(str, "continue")||
+           S_EQ(str, "switch")||
+           S_EQ(str, "case")||
+           S_EQ(str, "default")||
+           S_EQ(str, "goto")||
+           S_EQ(str, "typedef")||
+           S_EQ(str, "const")||
+           S_EQ(str, "extern")||
+           S_EQ(str, "restrict");
+}
 static struct token *token_make_operator_or_string()
 {
     char op=peekc();
     if(op=='<'){
         struct token* last_token=lexer_last_token();
-        //处理形如"#include<lyf.h>"的情况
+        //处理形如 #include<lyf.h> 的情况
         if(token_is_keyword(last_token,"include")){
             return token_make_string('<','>');
         }
@@ -254,7 +287,9 @@ static struct token* token_make_identifier_or_keyword(){
     buffer_write(buffer,0x00);
 
     //检查是否是关键字
-
+    if(is_keyword(buffer_ptr(buffer))){
+        return token_create(&(struct token){.type=TOKEN_TYPE_KEYWORD,.sval=buffer_ptr(buffer)});
+    }
     return token_create(&(struct token){.type=TOKEN_TYPE_IDENTIFIER,.sval=buffer_ptr(buffer)});
 }
 
